b87221378867f1c7e121982bfd3dfeef2cb8c221
toadermonica <toadermoncica@gmail.com>
2020-10-04 19:18:41 +0200

    https self generated certificate

--- index.js
+++ index.js
@@ -1,42 +1,28 @@
+import fs from 'fs';
+import https from 'https';
 import express from 'express';
-import chalk from 'chalk';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import usersApi from "./APIs/UsersApi";
-
 // Set up the express app
+const certificate = fs.readFileSync('./sslcert/certificate.pem');
+const key = fs.readFileSync('./sslcert/key-pkcs8.pem');
+const credentials = {key: key, cert: certificate, passphrase:'Pinguinireci2828'};
 const app = express();
-const port = process.env.PORT || 3333;
-
+// App settings and server
 app.use(cors());
 app.use(bodyParser.json({limit: '50mb'}) );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({                   // to support URL-encoded bodies
     extended: true,
     limit: '50mb'
 }));
-
 // API calls
 app.use('/users', usersApi);
-
 // Restful APIs
 app.get('/', function(req, res) {
     res.status(200).send('we are on! :)');
 });
+//set server start
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(8443);
 
-app.listen(port);
-console.log(chalk.bgGreenBright.bold('listening on: ' + port));
-
-/*// get all todos
-app.get('/api/v1/todos', (req, res) => {
-    res.status(200).send({
-        success: 'true',
-        message: 'todos retrieved successfully',
-
-    })
-});
-
-const PORT = 5000;
-
-app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`)
-});*/
